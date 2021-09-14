6f6db652c787047b3fc17017357e5093c436f089
yeonjooyou <yeonjooyou@naver.com>
2021-09-14 09:03:28 +0900

    Add solution to problem 5543

---
+++ baekjoon/Python/5543.py
@@ -0,0 +1,8 @@
+# 상근날드
+burger1 = int(input())
+burger2 = int(input())
+burger3 = int(input())
+coke = int(input())
+sprite = int(input())
+
+print(min(burger1, burger2, burger3) + min(coke, sprite) - 50)
